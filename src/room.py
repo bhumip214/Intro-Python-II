56f0b9667b82aea52fc6fea707b735d9880e5b9c
bhumip214 <bhumip214@gmail.com>
2019-04-24 13:04:46 -0700

    updated Room class with list and added add_items functionality

--- src/room.py
+++ src/room.py
@@ -2,6 +2,17 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, list):
         self.name = name
         self.description = description
+        # The Room class should be extended with a list that holds the Items that are currently in that room.
+        self.list = []
+
+    # Add the ability to add items to rooms.
+    def add_items(self, item):
+        self.list.append(item)
+
+    # Add functionality to the main loop that prints out all the items that are visible to the player when they are in that room.
+    def print_items(self):
+        for i in self.list: 
+            print(i.name)
